112df14e11192730cb91b7981f36736102f16786
Nurlybek316728 <utebekaigerim@gmail.com>
2022-10-21 16:35:07 +0600

    header

---
+++ src/component/Header/Header.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import "../Header/Header.css"
+import logo from "../../img/logo.svg"
+import search from "../../img/icon-search.svg"
+import cabinet from "../../img/icon-user.svg"
+import cart from "../../img/icon-cart.svg"
+const Header = () => {
+  return (
+    <header className="header d-flex align-items-center">
+      <div className="container container-xxl">
+        <div className='d-flex justify-content-between align-items-center'>
+          <div className='header__column header__column--first'>
+            <div className='header__subtitle'>
+              Косметика <br></br> из натуральных масел
+            </div>
+          </div>
+          <div className='header__column header__column--center text-center'>
+            <a className='header__logo img' href=''>
+              <img src={logo}></img>
+            </a>
+          </div>
+          <div className='header__column'>
+            <div className='header__buttons d-flex justify-content-end'>
+              <div className='search-button'>
+                <a href='#'>
+                  <img src={search}>
+                  </img>
+                </a>
+              </div>
+              <div className='cabinet-button'>
+                <a href='#'>
+                  <img src={cabinet}>
+                  </img>
+                </a>
+              </div>
+              <div className='cart-button'>
+                <a href='#'>
+                  <img src={cart}>
+                  </img>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
