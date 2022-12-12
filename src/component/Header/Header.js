a496583df71bf514091bf4dac3fef482cb5c49e0
Nurlybek316728 <utebekaigerim@gmail.com>
2022-12-13 02:31:37 +0600

    changed design

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -4,7 +4,7 @@ import logo from "../../img/logo.svg"
 import search from "../../img/icon-search.svg"
 import cabinet from "../../img/icon-user.svg"
 import cart from "../../img/icon-cart.svg"
-import { NavLink, useLocation} from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import Modal from '../Modal/Modal'
 import { Context } from '../..'
 import { useContext } from 'react'
@@ -20,26 +20,60 @@ const Header = () => {
         navigation.pathname != "/login" && navigation.pathname != "/reset" ?
           <header className="header d-flex align-items-center" >
             <div className="container container-xxl">
-              <div className='d-flex justify-content-between align-items-center'>
-                <div className='header__column header__column--first'>
+              <div className=' d-flex justify-content-between align-items-center'>
+                <div className='header-mobile header__column header__column--first'>
+                  <button style={{ backgroundColor: 'transparent' }} onClick={() => setModal(true)}>
+                    <img src={search} >
+                    </img>
+                  </button>
+                </div>
+                <div className='header_computer header__column header__column--first'>
                   <div className='header__subtitle' data-da="header__column--center,last,768" data-da-index="0">
                     Косметика <br></br> из натуральных масел
                   </div>
                 </div>
-                <div className='header__column header__column--center text-center'>
+                <div className='header-mobile  header__column header__column--center text-center'>
+                  <NavLink to='/' className='header__logo img' >
+                    <img src={logo}></img>
+                  </NavLink>
+                  <div className='header__subtitle' data-da="header__column--center,last,768" data-da-index="0">
+                    Косметика  из натуральных масел
+                  </div>
+                </div>
+                <div className='header_computer header__column header__column--center text-center'>
                   <NavLink to='/' className='header__logo img' >
                     <img src={logo}></img>
                   </NavLink>
                 </div>
-                <div className='header__column'>
+                <div className='header-mobile header__column'>
+                  <div className='header__buttons d-flex justify-content-end'>
+                  <div className='cabinet-button'>
+                      <NavLink to='/login'>
+                        <img src={cabinet}>
+                        </img>
+                      </NavLink>
+                    </div>
+                    <div className='cart-button'>
+                      <NavLink to='/order' style={{ display: 'flex', alignItems: "flex-end" }}>
+                        <img src={cart}>
+                        </img>
+                        {basket.Basket.length > 0
+                          ? <div className='box-order'>
+                            {basket?.Basket?.length}
+                          </div>
+                          : null
+                        }
+
+                      </NavLink>
+                    </div>
+                  </div>
+                </div>
+                <div className='header_computer header__column'>
                   <div className='header__buttons d-flex justify-content-end'>
                     <div className='search-button' data-da="header__column--first,first,768" data-da-index="1">
                       <button style={{ backgroundColor: 'transparent' }} onClick={() => setModal(true)}>
-
                         <img src={search} >
                         </img>
-
-
                       </button>
                     </div>
                     <div className='cabinet-button'>
@@ -49,21 +83,23 @@ const Header = () => {
                       </NavLink>
                     </div>
                     <div className='cart-button'>
-                      <NavLink to='/order' style={{ display: 'flex',alignItems:"flex-end" }}>
+                      <NavLink to='/order' style={{ display: 'flex', alignItems: "flex-end" }}>
                         <img src={cart}>
                         </img>
-                        {basket.Basket.length>0
-                        ? <div className='box-order'>
-                        {basket?.Basket?.length}
-                        </div>
-                        :null
+                        {basket.Basket.length > 0
+                          ? <div className='box-order'>
+                            {basket?.Basket?.length}
+                          </div>
+                          : null
                         }
-                        
+
                       </NavLink>
                     </div>
                   </div>
                 </div>
               </div>
+
+
               <Modal active={modal} setActive={setModal}>
                 <div className='fancybox__content'>
                   <form>
