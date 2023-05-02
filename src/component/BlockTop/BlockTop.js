2256d632bc6705cd31d9d453d2448f4548412a42
Numbone <nurlybekqwerty@gmail.com>
2023-05-02 13:59:17 +0600

    block top

--- src/component/BlockTop/BlockTop.js
+++ src/component/BlockTop/BlockTop.js
@@ -1,34 +1,32 @@
-import { observer } from 'mobx-react-lite'
-import React, { useContext } from 'react'
-import { Context } from '../..'
-import "../BlockTop/BlockTop.css"
+import { observer } from "mobx-react-lite";
+import React, { useContext } from "react";
+import { Context } from "../..";
+import "../BlockTop/BlockTop.css";
 const BlockTop = () => {
-    const { lang } = useContext(Context)
-    return (
-
-        <div className='block-top'>
-            <div className='d-flex justify-content-center'>
-                <div className='container flex-1 d-flex align-items-end justify-content-md-end'>
-                    <div className='block-top__badge-box'>
-
-                        {
-                            lang.lang === 'ru'
-                                ?
-                                <div className='block-top__badge'>
-                                    Позвольте себе ощутить истинное наслаждение с косметикой из ценных натуральных масел. Ваша кожа ещё никогда не была такой увлажненной и напитанной.
-
-                                </div>
-                                :
-                                <div className='block-top__badge'>
-                                    Let yourself experience true pleasure with cosmetics made from precious natural oils. Your skin has never been so hydrated and nourished.
-                                </div>
-                        }
-
-                    </div>
-                </div>
-            </div>
+  const { lang } = useContext(Context);
+  return (
+    <div className="block-top" >
+      <div className="d-flex justify-content-center">
+        <div className="container flex-1 d-flex align-items-end justify-content-md-end">
+          <div className="block-top__badge-box">
+            {lang.lang === "ru" ? (
+              <div className="block-top__badge">
+                Позвольте себе ощутить истинное наслаждение с косметикой из
+                ценных натуральных масел. Ваша кожа ещё никогда не была такой
+                увлажненной и напитанной.
+              </div>
+            ) : (
+              <div className="block-top__badge">
+                Let yourself experience true pleasure with cosmetics made from
+                precious natural oils. Your skin has never been so hydrated and
+                nourished.
+              </div>
+            )}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default observer(BlockTop)
+export default observer(BlockTop);
